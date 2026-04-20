77b555a8dc0f821d90ad5635744690066ff0a071
cross1mn <cross1mn@cmich.edu>
2026-04-20 13:43:52 -0400

    boooo

--- Labs/lab14-variables,attributes,etc/JS/script.js
+++ Labs/lab14-variables,attributes,etc/JS/script.js
@@ -3,7 +3,7 @@ console.log("Java Script is nice");
 
 // Open the overlay 
 function openOverlay () {
-    document.getElementById("overlay-menu").style.width = "100vw";
+    document.getElementById("overlay-menu").style.width = "100vw" .style.height = "100vh";
 }
 
 // Close the overlay 
