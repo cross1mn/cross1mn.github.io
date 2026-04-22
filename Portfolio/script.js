4bf2b80eb1119186382482460141d2c79a036d63
cross1mn <cross1mn@cmich.edu>
2026-04-22 13:32:35 -0400

    scared

---
+++ Portfolio/script.js
@@ -0,0 +1,44 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 2,
+//     spaceBetween: 30,
+      freeMode: true,
+      grabCursor: true,
+
+  // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+  autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+
+  // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+console.log("Java Script is nice");
+
+
+// Open the overlay 
+function openOverlay () {
+    document.getElementById("overlay-menu").style.width = "100vw" .style.height = "100vh";
+}
+
+// Close the overlay 
+function closeOverlay () {
+    document.getElementById("overlay-menu").style.width = "0";
+}
