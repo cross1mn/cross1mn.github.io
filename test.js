a361cd9e619b88adc60a8df74ae233b49fe04f7e
cross1mn <cross1mn@cmich.edu>
2026-07-20 22:52:21 -0400

    kms

---
+++ test.js
@@ -0,0 +1,13 @@
+// Close the overlay 
+function closeOverlay () {
+    document.getElementById("overlay-menu").style.width = "0px";
+}
+
+// Open the overlay 
+function openOverlay () {
+    document.getElementById("overlay-menu").style.width = "100vw";
+}
+
+// function openOverlay () {
+//     document.getElementById("overlay-menu").style.width = "100vh";
+// }
